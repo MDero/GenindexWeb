5ab953584e540ece2d5edf4215ffc1394d0b7c1a
Mouuuahaha <caroline.petit.cp@gmail.com>
2014-05-12 15:07:15 +0200

    Interface et InterfaceCat par Caro

---
+++ src/java/interfaceSwing/CreateCategory.java
@@ -0,0 +1,87 @@
+package interfaceSwing;
+
+import kernel.*;
+
+import java.awt.*; 
+import javax.swing.*;
+import java.awt.event.*;
+
+
+/**
+ *
+ * @author Caro
+ */
+public class CreateCategory extends JFrame {
+
+
+        // Déclaration des variables
+        private JMenuBar menuBar;
+        private JMenuItem Exit;
+
+        private JButton valider=new JButton("Valider ");
+        // le label explicatif
+        private JLabel nom, caracteristiques;
+        // le label permettant l'écho
+        private JLabel echo;
+        // zone de saisie
+        private JTextField Cnom, Ccaracteristiques;
+        // zone de placement?
+        private JPanel zone1, zone2, champs, bouton;
+    
+    public CreateCategory (){
+        JFrame myFrame = new JFrame("Specie");
+        //Création de la barre de menu
+        menuBar = new JMenuBar();
+        
+        //Installation dans la fenêtre
+        this.setJMenuBar(menuBar);
+        
+        //Construction du premier menu
+        JMenu menu = new JMenu("Menu");
+        menu.setMnemonic(KeyEvent.VK_M);
+        menuBar.add(menu);  
+        Exit = new JMenuItem ("Exit",KeyEvent.VK_D);
+        menu.add(Exit);
+        
+        // creation des widgets
+        nom = new JLabel("Nom*:");
+        caracteristiques = new JLabel("Caractéristiques :");
+        
+        Cnom = new JTextField(20);
+        Ccaracteristiques = new JTextField(20);
+        
+        zone1 = new JPanel();
+        zone1.setLayout(new GridLayout (1,2));
+        zone1.add(nom);
+        zone1.add(Cnom);
+        
+        zone2 = new JPanel();
+        zone2.setLayout(new GridLayout (1,2));
+        zone2.add(caracteristiques);
+        zone2.add(Ccaracteristiques);
+        
+        champs = new JPanel(); 
+        champs.setLayout(new GridLayout(3,1));
+        champs.add(zone1);
+        champs.add(zone2);
+        
+        bouton = new JPanel();
+        bouton.add(valider);
+        
+        
+  
+        myFrame.setLayout(new BorderLayout());
+        myFrame.add(menuBar, BorderLayout.NORTH);
+        myFrame.add(champs, BorderLayout.CENTER);
+        myFrame.add(bouton, BorderLayout.SOUTH);;
+        myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+         myFrame.pack();
+        myFrame.setVisible(true);
+    }
+    
+        public static void main (String [] args){
+CreateCategory visuel= new CreateCategory();
+}
+}
+    
+
