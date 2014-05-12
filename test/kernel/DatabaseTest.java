fc1962858bbd05e36546b9e76e5fe73fd59fcf05
MDero <maxime.derobillard@gmail.com>
2014-05-12 11:40:23 +0200

    Redo DatabaseTest.java

--- test/kernel/DatabaseTest.java
+++ test/kernel/DatabaseTest.java
@@ -1,39 +1,53 @@
 package kernel;
 
+import java.sql.SQLException;
 import java.sql.SQLIntegrityConstraintViolationException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import junit.framework.TestCase;
+import org.junit.After;
 
 public class DatabaseTest extends TestCase {
-	
-	//this test permits to check if the function listOrder return the orders in the database
-	Database d = new Database();
-        //TODO: Test ADRESS insert
-        
-        
-        //CUSTOMERS TESTS
-        //MDERO
-        public void InsertCustomer() { //VALIDATED BY MDERO
-            //public Customers(String first, String last, Adress adress, String phone, int ID, int typeCusto) {
-            //  public Adress(int id, int number, String street, int zip, String city, String country) {
-           // try {
-            Customers cTest = new Customers("Maxime", "Dero",new Adress (2,3,"rue du petit four",99999,"Poitiers","France"), "0699644317",10);
-            d.insertCustomerWOID(cTest);
-           // }
-           // catch(SQLIntegrityConstraintViolationException e) {
-           //System.out.println("Déjà ajouté !");
-           // }
-        }
-        //MDero
-        //TODO:Fix dat
-        public void testGetCustomer() {
-            Customers c =  d.getCustomer(0);
-        }
-	
-        
-        
-	
-	//There is only one order in the database, and the size of the list is 1 so the test is successful.
 
+
+    Database d = new Database();
+    Adress aTest = new Adress(100, 3, "rue du petit four", 99999, "Poitiers", "France");
+    Customers cTest = new Customers("Maxime", "Dero", aTest, "0699644317", 99, 10);
+
+
+    //ADDRESS TESTS
+    //MDERO
+    public void testInsertAdress() {
+        //public Adress(int id, int number, String street, int zip, String city, String country) {
+        d.insertAdress(aTest);
+    }
+    
+    
+    //CUSTOMERS TESTS
+    
+    //MDERO
+    public void testDelCustomer() {
+        d.delCustomer(99);
+    }
+
+    //MDERO
+    public void testInsertCustomer() { //VALIDATED BY MDERO
+        //public Customers(String first, String last, Adress adress, String phone, int ID, int typeCusto) {
+        d.insertCustomerWOID(cTest);
+    }
+
+    //MDero
+    public void testGetCustomer() {
+        Customers c = d.getCustomer(0);
+    }
+
+    @After
+    public void after() {
+        try {
+            d.connexion.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(DatabaseTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
 }
-
-
