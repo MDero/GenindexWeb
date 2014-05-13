75991b004a8cb0c92f24596e3a42f1587248bb3f
Mouuuahaha <caroline.petit.cp@gmail.com>
2014-05-13 17:46:50 +0200

    Interface Create Microplate - Caro Elo

--- src/java/interfaceSwing/CreateMicroplate.java
+++ src/java/interfaceSwing/CreateMicroplate.java
@@ -5,7 +5,8 @@
  
 package interfaceSwing;
 
-import static interfaceSwing.CreateSpecie.database;
+
+import static interfaceSwing.CreateMicroplate.database;
 import java.awt.*; 
 
 import javax.swing.*;
@@ -16,6 +17,9 @@ import kernel.Database;
 import kernel.TypeAnalysis;
 import kernel.TypeSample;
 import java.util.Vector;
+import javax.swing.table.DefaultTableModel;
+import kernel.Customers;
+import kernel.Orders;
 import kernel.Samples;
 
 /*
@@ -23,7 +27,7 @@ import kernel.Samples;
  * @author Caro
  */
 
-public class CreateMicroplate extends JFrame {
+public class CreateMicroplate extends JFrame implements ActionListener {
     
         //database
         static Database database = new Database();
@@ -33,13 +37,15 @@ public class CreateMicroplate extends JFrame {
         private JFrame myFrame;
         private JMenuItem exit;
         private JLabel tanal, tsample;
-        private JComboBox boxCategory;
+        private JComboBox boxAnalyse;
         private String[] items;
         private String[] ech;
         private JList listEch;
         private ListSelectionModel listEchSelectionModel;
         private Vector imageList;
         private JPanel zone1, zone2,zone3;
+        private JTable table;
+        private int lastEmptyRow=0;
         
         public CreateMicroplate (){
         JFrame myFrame = new JFrame("Create microplate");
@@ -77,8 +83,8 @@ public class CreateMicroplate extends JFrame {
             items[i]=typeAnalNames.get(i);
         
                 //création effective 
-        this.boxCategory = new JComboBox(items);
-        
+        this.boxAnalyse = new JComboBox(items);
+        boxAnalyse.addActionListener(this);
         
         
         /* creation de la liste d'échantillon */
@@ -100,17 +106,27 @@ public class CreateMicroplate extends JFrame {
         imageList=new Vector();
         listEch = new JList(ech);
         
-
-        zone1= new JPanel();
-        zone1.setLayout(new GridLayout (1,2));
-        zone1.add(tanal);
-        zone1.add(boxCategory);
+        //TABLE D ORDERS
+        table = new JTable(new DefaultTableModel(new Object[]{"Numéro","Client","Priorité"},10));
+        lastEmptyRow=10;//de base on a 10 lignes pour le swag
         
         zone2 = new JPanel();
         zone2.setLayout(new GridLayout (1,2));
         zone2.add(tsample);
-        zone2.add(listEch);
+        zone2.add(table);
+        
+        JScrollPane scrollPane = new JScrollPane(table);
+        scrollPane.setPreferredSize(new Dimension(800,100));
+	scrollPane.setColumnHeaderView(table.getTableHeader());
+	zone2.add(scrollPane);
 
+
+        zone1= new JPanel();
+        zone1.setLayout(new GridLayout (1,2));
+        zone1.add(tanal);
+        zone1.add(boxAnalyse);
+        
+        
 
         myFrame.add(menuBar, BorderLayout.NORTH);
         myFrame.add(zone1, BorderLayout.WEST);
@@ -118,11 +134,66 @@ public class CreateMicroplate extends JFrame {
 
         myFrame.pack();
         myFrame.setVisible(true);
+        
+        
 }
+        @Override
+	public void actionPerformed(ActionEvent e) {
 
+		// TODO Auto-generated method stub
+
+				JComboBox cb = (JComboBox)e.getSource();
+                                String[] items;
+                                
+                                ArrayList<Samples> samples = (ArrayList<Samples>) database.getSampleList();
+                                ArrayList<Samples> samplesToAnalyze = new ArrayList<>();
+                                ArrayList<Samples> samplesPriority = new ArrayList<>();
+                                for (Samples sample : samples){
+                                    //0 is analysed 1 is to analyse 2 is analysed but need to re analyse 
+                                    if(sample.getStatusId()==2){
+                                        samplesPriority.add(sample);
+                                    }
+                                    else if(sample.getStatusId()==1){
+                                        samplesToAnalyze.add(sample);
+                                    }
+                                }
+                                
+                                //clear screen
+                                for (int i = 0 ; i<CreateMicroplate.this.lastEmptyRow;i++)
+                                     ((DefaultTableModel)CreateMicroplate.this.table.getModel()).removeRow(0);
+                                CreateMicroplate.this.lastEmptyRow=0;
+                                //add new rows
+                                if (samplesPriority.size()>0){
+                                    for (Samples sample : samplesPriority){
+                                        //find the customer
+                                        Orders order = sample.getOrder();
+                                        Customers customer = order.getCustomer();
+                                        
+                                        ((DefaultTableModel)CreateMicroplate.this.table.getModel()).addRow(new Object[]{sample.getId(),customer.getFirstName()+" "+customer.getLastName(),"PRIORITAIRE"});
+                                        lastEmptyRow++;
+                                    }
+                                }
+                                if (samplesToAnalyze.size()>0){
+                                    for (Samples sample : samplesToAnalyze){
+                                        //find the customer
+                                        Orders order = sample.getOrder();
+                                        Customers customer = order.getCustomer();
+                                        
+                                        ((DefaultTableModel)CreateMicroplate.this.table.getModel()).addRow(new Object[]{sample.getId(),customer.getFirstName()+" "+customer.getLastName(),""});
+                                        lastEmptyRow++;
+                                    }
+                                }
+                                
+                                
+//                                if (typeAnalNames.size()>0){
+//                                    for (TypeAnalysis typeanal : typeAnalNames){
+//                                        ((DefaultTableModel)CreateMicroplate.this.table.getModel()).addRow(new Object[]{typeanal.getType(),order.getResultSend(),order.getPaid(),order.getNumberSamples()});                                    
+//                                        CreateMicroplate.this.lastEmptyRow++;
+//                                    }
+//                                }
+	}
            public static void main (String [] args){
 CreateMicroplate visuel= new CreateMicroplate();
-
            }
 }
            
